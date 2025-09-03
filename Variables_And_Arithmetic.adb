d2562f6093579a59fb2562464768669fde9fa91f
DearDorothy <deardorothy@mail.ru>
2025-09-03 16:08:46 +0300

    update the file Variables_And_Arithmetic.adb

--- Variables_And_Arithmetic.adb
+++ Variables_And_Arithmetic.adb
@@ -2,7 +2,7 @@
 --  Содержание:
 --    1. Объявление и инициализация переменных
 --    2. Арифметические операции (+,-,*,/)
---    3. Операции сравнения (=, <, >, <=, >=)
+--    3. Операции сравнения (=, /=,  <, >, <=, >=)
 --    4. Операция присваивания (:=)
 
 with Ada.Text_IO; use Ada.Text_IO; -- Пакет для ввода/вывода
@@ -10,6 +10,8 @@ with Ada.Text_IO; use Ada.Text_IO; -- Пакет для ввода/вывода
 procedure Variables_And_Arithmetic is
 
    -- 1. ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ РАЗНЫХ ТИПОВ
+   -- В названии переменной допустимы буквы (a-z, A-Z), цифры (0-9) и нижнее подчеркивнаие (_)
+   -- !!! Но первый символ, в названии перменной, доджен быть буквой (a-z, A-Z) 
 
    -- Целое число
    Integer_Number : Integer := 10;
@@ -27,8 +29,6 @@ procedure Variables_And_Arithmetic is
 
 begin
 
-   Value_Boolean : Boolean := False;
-
    Put_Line("=== НАЧАЛО ПРОГРАММЫ ===");
    New_Line;
 
@@ -37,7 +37,57 @@ begin
    Put_Line("Integer_Number: " & Integer'Image(Integer_Number));
    Put_Line("Character_Symbol: " & Character_Symbol);
    Put_Line("String_Text: " & String_Text);
-   Put_Line("Value_Boolean: " & Boolean'Image(Value_Boolean));
+   New_Line;
+
+   -- 3. АРИФМЕТИЧЕСКИЕ ОПЕРАЦИИ
+   Put_Line("=== АРИФМЕТИЧЕСКИЕ ОПЕРАЦИИ ===");
+
+   A := 20;
+   B := 5;
+
+   -- Сложение 
+   Result_Add := A + B;
+   Put_Line(Integer'Image(A) & " + " & Integer'Image(B) & " = " & Integer'Image(Result_Add));
+
+   -- Вычитание
+   Result_Sub := A - B;
+   Put_Line(Integer'Image(A) & " - " & Integer'Image(B) & " = " & Integer'Image(Result_Sub));
+   
+   -- Умножение
+   Result_Mul := A * B;
+   Put_Line(Integer'Image(A) & " * " & Integer'Image(B) & " = " & Integer'Image(Result_Mul));
+   
+   -- Деление (ЦЕЛОЧИСЛЕННОЕ!)
+   Result_Div := A / B; -- 20 / 5 = 4
+   Put_Line(Integer'Image(A) & " / " & Integer'Image(B) & " = " & Integer'Image(Result_Div));
+   New_Line;
+
+   -- 4. ОПЕРАЦИИ СРАВНЕНИЯ
+   Put_Line("=== ОПЕРАЦИИ СРАВНЕНИЯ ===");
+   
+   Is_Equal := (A = B); -- False
+   Put_Line(Integer'Image(A) & " = " & Integer'Image(B) & " ? " & Boolean'Image(Is_Equal));
+   
+   Is_Equal := (A /= B); -- True
+   Put_Line(Integer'Image(A) & " /= " & Integer'Image(B) & " ? " & Boolean'Image(Is_Equal));
+   
+   Is_Greater := (A > B); -- True
+   Put_Line(Integer'Image(A) & " > " & Integer'Image(B) & " ? " & Boolean'Image(Is_Greater));
+   
+   Is_Greater := (A < B); -- False
+   Put_Line(Integer'Image(A) & " < " & Integer'Image(B) & " ? " & Boolean'Image(Is_Greater));
+   New_Line;
+
+   -- 5. ОПЕРАЦИЯ ПРИСВАИВАНИЯ
+   Put_Line("=== ОПЕРАЦИЯ ПРИСВАИВАНИЯ ===");
+   
+   Put_Line("Integer_Number До присваивания: " & Integer'Image(Integer_Number));
+   Integer_Number := 42; -- Присваивание нового значения
+   Put_Line("Integer_Number После присваивания: " & Integer'Image(Integer_Number));
+   
+   -- Множественное присваивание (через промежуточные операции)
+   Integer_Number := A + B * 2; -- Сначала вычисляется выражение, потом присваивание
+   Put_Line("Результат выражения A + B * 2: " & Integer'Image(Integer_Number));
    New_Line;
 
 end Variables_And_Arithmetic;

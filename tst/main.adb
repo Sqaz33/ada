d735cc5787f09573b1e59c6ad020232251d83250
Matveev Stepan <mset321@gmail.com>
2025-09-07 13:33:10 +0300

    reafactor: tst

--- tst/main.adb
+++ tst/main.adb
@@ -6,9 +6,11 @@ procedure Main is
    x : Integer := 0;                          
    y : Integer := 0;                          
    res : Integer := 0;                          
+   res2 : Integer := 0;                          
 begin                                      
    x := 1;
    y := 2;
    res := T.Test(x, y);
+   res2 := T.Test2(x, y);
    Put_Line("1 + 2 =" & Integer'Image(X));   
 end Main;                                  
--- tst/t.ads
+++ tst/t.ads
@@ -1,3 +1,4 @@
 package T is 
    function Test(a, b: Integer) return Integer;
+   function Test2(a, b: Integer) return Integer;
 end T;

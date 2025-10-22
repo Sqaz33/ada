2139f0430b0f10cc1e246bdba1d64820f66b2b48
Matveev Stepan Andreevich <mset321@gmail.com>
2025-10-22 11:05:35 +0300

    feat: oop update

--- oop/main.adb
+++ oop/main.adb
@@ -1,15 +1,28 @@
 with Pack;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
+   procedure Bar(B: Pack.Base) is
+   begin
+      Put_Line("Base");
+   end Bar;
+
+   procedure Bar(B: Pack.Derived) is
+   begin
+      Put_Line("Derived");
+   end Bar;
 
    procedure Pol(B: Pack.Base'Class) is
       M: Pack.My_Class;
    begin
       Pack.Foo(1, B, B, M);
+      --  Bar(B); --  Cannot call Bar(B): not a dispatching operation of Base.
    end Pol;
    V1: Pack.Base := (X => 2);
    V2: Pack.Derived := (X => 3);
    V3: Pack.DerivedDerived := (X => 4);
+
+
 
 begin
 
